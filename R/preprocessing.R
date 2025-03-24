a948bbf51d5671b7237cdc2d46119145d6a1c946
Chenghao Wang <wangchenghaochn@gmail.com>
2025-03-24 21:46:45 +0800

    A function for lognormalizing count matrix

--- R/preprocessing.R
+++ R/preprocessing.R
@@ -1,4 +1,13 @@
 
 
 
-log_normalize <- function(count_matrix, scale.factor = 10000, base = exp(1))
+log_normalize <- function(count_matrix, MARGIN = 2L, scale_factor = 10000, base = exp(1)) {
+  
+  lib_size <- switch(as.character(MARGIN),
+                     "1" = rowSums(count_matrix),
+                     "2" = colSums(count_matrix),
+                     stop("'MARGIN' must be 1 or 2"))
+  expression_matrix <- logb(sweep(count_matrix, MARGIN = MARGIN, STATS = lib_size, FUN = "/") * scale_factor + 1, base = base)
+  return(expression_matrix)
+}
+
